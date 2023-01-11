cd708e9da4dc13df6420f99fc4f62d1187a7cf41
Admir Bajric <admir.bajric.dev@gmail.com>
2023-01-11 20:56:25 +0100

    Updated all files

---
+++ main.js
@@ -0,0 +1,20 @@
+const form = document.querySelector(".form");
+const input = document.querySelector(".email");
+const error = document.querySelector(".error");
+const errorTxt = document.querySelector(".error-txt");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (validateEmail(input)) {
+    error.style.opacity = 0;
+    errorTxt.style.opacity = 0;
+  } else {
+    error.style.opacity = 1;
+    errorTxt.style.opacity = 1;
+  }
+});
+
+const validateEmail = (inputText) => {
+  var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return inputText.value.match(mailFormat);
+};
